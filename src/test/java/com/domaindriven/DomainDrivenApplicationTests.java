4c3ddfb5c6863247e0f7413af7b34f99ca891b3b
Minjae Kim <minjae.kim@mytrum.com>
2022-09-12 09:18:32 +0900

    Initial commit

---
+++ src/test/java/com/domaindriven/DomainDrivenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domaindriven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainDrivenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
