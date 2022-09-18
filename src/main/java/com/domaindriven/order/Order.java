04f1462cbad2ed35aa68ea2b41d50af34974f396
Minjae Kim <minjae.kim@mytrum.com>
2022-09-18 09:21:35 +0900

    refactor: Order Domain Refactoring
    * 객체 출력을 위한 toString 메소드 생성
    * AllArgs 생성자 생성
    * Jpa 객체 생성을 위한 protected NoArgs 생성자 생성

--- src/main/java/com/domaindriven/order/Order.java
+++ src/main/java/com/domaindriven/order/Order.java
@@ -1,5 +1,6 @@
 package com.domaindriven.order;
 
+import javax.persistence.Access;
 import javax.persistence.AttributeOverride;
 import javax.persistence.AttributeOverrides;
 import javax.persistence.Column;
@@ -8,6 +9,12 @@ import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 @Entity
 @Table(name = "D_ORDER")
 public class Order {
@@ -24,4 +31,10 @@ public class Order {
             @AttributeOverride(name = "zipCode", column = @Column(name = "ZIPCODE"))
     })
     private ShippingInfo shippingInfo;
+
+    @Override
+    public String toString() {
+        return "orderId:" + orderId.orderIdString() + " address:" + shippingInfo.address() +
+               " zipCode:" + shippingInfo.zipCode();
+    }
 }
--- src/main/java/com/domaindriven/order/ShippingInfo.java
+++ src/main/java/com/domaindriven/order/ShippingInfo.java
@@ -2,9 +2,22 @@ package com.domaindriven.order;
 
 import javax.persistence.Embeddable;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 @Embeddable
 public class ShippingInfo {
 
     private String address;
     private String zipCode;
+
+    protected ShippingInfo() {}
+
+    public String address() {
+        return address;
+    }
+
+    public String zipCode() {
+        return zipCode;
+    }
 }
