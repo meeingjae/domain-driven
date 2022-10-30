e49780732098ece6b66b4e90c3f1ee7b80f2f6f3
Minjae Kim <minjae.kim@mytrum.com>
2022-10-30 09:36:26 +0900

    feat: spring data jpa 잠금 모드 및 쿼리 힌트 구현
    * ```@Lock``` Annotation
    * ```@QueryHints``` annotation

--- src/main/java/com/domaindriven/article/ArticleWriterRepository.java
+++ src/main/java/com/domaindriven/article/ArticleWriterRepository.java
@@ -1,8 +1,17 @@
 package com.domaindriven.article;
 
+import javax.persistence.LockModeType;
+import javax.persistence.QueryHint;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.QueryHints;
 
 public interface ArticleWriterRepository extends JpaRepository<ArticleWriter, Long> {
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @QueryHints({
+            @QueryHint(name = "javax.persistence.lock.timeout", value = "2000")
+    })
     ArticleWriter findArticleWriterByArticleId(long articleId);
 }
